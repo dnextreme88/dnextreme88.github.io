a8e249de8924260ef3fef4f800bcbbfe96c225ed
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2024-01-18 15:21:08 -0500

    add new sold items

--- archives/archives_items.js
+++ archives/archives_items.js
@@ -1900,6 +1900,14 @@ const items = [
         dateSold: '2024-01-13',
         imageLocation: '2024/apple3-1.jpg'
     },
+    {
+        name: 'Apple iPhone 5 Phone',
+        price: 100,
+        condition: 'used',
+        size: 'N/A',
+        dateSold: '2024-01-14',
+        imageLocation: '2024/apple4-1.jpg'
+    },
 ];
 
 const brands = [
